3bcc9693ad905df7022c02749545bee97402b7e4
itssujalcoder24 <sujaldas9702@gmail.com>
2026-01-28 01:46:14 +0530

    Support tkt Classification using ML

--- demo/demo_app.py
+++ demo/demo_app.py
@@ -7,8 +7,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.preprocess import clean_text
 from src.priority_logic import assign_priority
 
-
-
 tfidf = joblib.load("models/tfidf_vectorizer.pkl")
 model = joblib.load("models/ticket_classifier.pkl")
 
@@ -17,15 +15,11 @@ print("-----------------------------------")
 
 while True:
     ticket = input("\nEnter support ticket (or 'exit'): ")
-
     if ticket.lower() == "exit":
         break
-
     cleaned = clean_text(ticket)
     vector = tfidf.transform([cleaned])
-
     category = model.predict(vector)[0]
     priority = assign_priority(ticket)
-
     print("Predicted Category:", category)
     print("Predicted Priority:", priority)
